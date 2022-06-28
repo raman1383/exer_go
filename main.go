a23cd8854a49c9c0c5beec49f9dadf18958e91e2
raman1383 <raman83shariati@gmail.com>
2022-06-28 22:46:23 +0430

    e

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	number1 := 12
+	number2 := 20
+
+	if number1 == number2 {
+		fmt.Printf("Result: %d == %d", number1, number2)
+	} else if number1 > number2 {
+		fmt.Printf("Result: %d > %d", number1, number2)
+	} else {
+		fmt.Printf("Result: %d < %d", number1, number2)
+	}
+}
