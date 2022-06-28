d80b2ef1645b80eceacb7cbc0960de1cdfed979d
raman1383 <raman83shariati@gmail.com>
2022-06-28 23:03:38 +0430

    r

--- main.go
+++ main.go
@@ -3,43 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	number1 := 12
-	number2 := 20
-
-	if number1 == number2 {
-		fmt.Printf("Result: %d == %d", number1, number2)
-	} else if number1 > number2 {
-		fmt.Printf("Result: %d > %d", number1, number2)
-	} else {
-		fmt.Printf("Result: %d < %d", number1, number2)
+	num := 0
+	for num <= 10 {
+		fmt.Println(num)
+		num++
 	}
 
-	dayOfWeek := 3
-
-	switch dayOfWeek {
-
-	case 1:
-		fmt.Println("Sunday")
-
-	case 2:
-		fmt.Println("Monday")
-
-	case 3:
-		fmt.Println("Tuesday")
-
-	case 4:
-		fmt.Println("Wednesday")
-
-	case 5:
-		fmt.Println("Thursday")
-
-	case 6:
-		fmt.Println("Friday")
-
-	case 7:
-		fmt.Println("Saturday")
-
-	default:
-		fmt.Println("Invalid day")
-	}
 }
