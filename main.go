f8526e5a5f9133f6277504e881dcd3d10b41732f
raman1383 <raman83shariati@gmail.com>
2022-06-28 22:48:52 +0430

    e

--- main.go
+++ main.go
@@ -13,4 +13,33 @@ func main() {
 	} else {
 		fmt.Printf("Result: %d < %d", number1, number2)
 	}
+
+	dayOfWeek := 3
+
+	switch dayOfWeek {
+
+	case 1:
+		fmt.Println("Sunday")
+
+	case 2:
+		fmt.Println("Monday")
+
+	case 3:
+		fmt.Println("Tuesday")
+
+	case 4:
+		fmt.Println("Wednesday")
+
+	case 5:
+		fmt.Println("Thursday")
+
+	case 6:
+		fmt.Println("Friday")
+
+	case 7:
+		fmt.Println("Saturday")
+
+	default:
+		fmt.Println("Invalid day")
+	}
 }
